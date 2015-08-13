b67c28cfb441605c82f12ebd855a4b9f363d9231
Yucayeque <yucayeque@live.com>
2015-08-13 14:26:16 -0400

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/paycoin.png
-ICON_DST=../../src/qt/res/icons/paycoin.ico
-convert ${ICON_SRC} -resize 16x16 paycoin-16.png
-convert ${ICON_SRC} -resize 32x32 paycoin-32.png
-convert ${ICON_SRC} -resize 48x48 paycoin-48.png
-convert paycoin-48.png paycoin-32.png paycoin-16.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/lendcoin.png
+ICON_DST=../../src/qt/res/icons/lendcoin.ico
+convert ${ICON_SRC} -resize 16x16 lendcoin-16.png
+convert ${ICON_SRC} -resize 32x32 lendcoin-32.png
+convert ${ICON_SRC} -resize 48x48 lendcoin-48.png
+convert paycoin-48.png paycoin-32.png lendcoin-16.png ${ICON_DST}
 

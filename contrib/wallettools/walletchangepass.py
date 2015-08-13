c17932a8a41541265b88a38eaec11800193b49df
Yucayeque <yucayeque@live.com>
2015-08-13 15:35:05 -0400

    Update walletchangepass.py

--- contrib/wallettools/walletchangepass.py
+++ contrib/wallettools/walletchangepass.py
@@ -2,4 +2,4 @@ from jsonrpc import ServiceProxy
 access = ServiceProxy("http://127.0.0.1:8332")
 pwd = raw_input("Enter old wallet passphrase: ")
 pwd2 = raw_input("Enter new wallet passphrase: ")
-access.walletpassphrasechange(pwd, pwd2)
+access.walletpassphrasechange(pwd, pwd2) 

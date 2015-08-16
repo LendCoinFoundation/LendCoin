e36ea7a571dc4065748656e3bf9a9c44671e5b92
Yucayeque <yucayeque@live.com>
2015-08-16 18:15:07 -0400

    Update walletchangepass.py

--- contrib/wallettools/walletchangepass.py
+++ contrib/wallettools/walletchangepass.py
@@ -3,3 +3,4 @@ access = ServiceProxy("http://127.0.0.1:8332")
 pwd = raw_input("Enter old wallet passphrase: ")
 pwd2 = raw_input("Enter new wallet passphrase: ")
 access.walletpassphrasechange(pwd, pwd2) 
+//CIMS
